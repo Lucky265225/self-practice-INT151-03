f9c53cd06453f4b06676f38c64017f87ae150fa3
Student <Student@Train10-4-PC256.control.ad.sit.kmutt.ac.th>
2025-11-12 12:25:10 +0700

    week12

--- week11/quote-project/js/quoteManagement.js
+++ week11/quote-project/js/quoteManagement.js
@@ -1,16 +1,44 @@
-// CRUD on quotes
-import { getItems } from "./myLib/fetchUils";
-
-// GET
-async function loadQuotes(){
-    try{
-        const quotes = await getItems(`${import.meta.env.VITE_APP_URL}/quotes`)
-        console.log(quotes)
-        return quotes
-    }catch(error){
-        alert(error)
-    }
+//CRUD on quotes
+import { getItems, deleteItem, addItem, editItem } from "./myLib/fetchUtils.js"
+const quoteURL = `${import.meta.env.VITE_APP_URL}/quotes`
+//GET Quotes
+async function loadQuotes() {
+  try {
+    const quotes = await getItems(quoteURL)
+    console.log(quotes)
+    return quotes
+  } catch (error) {
+    alert(`Quote: ${error}`)
+  }
+}
+async function addQuote(item) {
+  try {
+    const addedQuote = await addItem(quoteURL, item)
+    return addedQuote
+  } catch (error) {
+    alert(`Quote: ${error}`)
+  }
 }
 
+async function editQuote(item) {
+  try {
+    const editedQuote = await editItem(quoteURL, item)
+    return editedQuote
+  } catch (error) {
+    alert(`Quote: ${error}`)
+  }
+}
 
-export {loadQuotes}
+async function deleteQuote(id) {
+  try {
+    const removeId = await deleteItem(quoteURL, id)
+    return removeId
+  } catch (error) {
+    alert(`Quote: ${error}`)
+  }
+}
+export { loadQuotes, deleteQuote, addQuote, editQuote }
+//Create Quote
+//Edit Quote
+//Delete Quote
+ 
